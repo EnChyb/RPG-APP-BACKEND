0c958ed22ae9f96695cf770b876d38903bdeb20b
Marta Tabor <leeloothefifth@googlemail.com>
2025-02-21 21:15:37 +0000

    update user logic add

--- src/controllers/User/index.ts
+++ src/controllers/User/index.ts
@@ -1,5 +1,6 @@
 import register from "./registerUser.js";
 import login from "./loginUser.js";
 import getUser from "./getUser.js";
+import updateUser from "./updateUser.js";
 
-export { register, login, getUser };
+export { register, login, getUser, updateUser };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { register, login, getUser } from "../controllers/User/index.js";
+import {
+  register,
+  login,
+  getUser,
+  updateUser,
+} from "../controllers/User/index.js";
 import protect from "../middlewares/authMiddleware.js";
 import upload from "../middlewares/uploadMiddleware.js";
 
@@ -8,5 +13,6 @@ const router = express.Router();
 router.post("/register", upload.single("avatar"), register);
 router.post("/login", login);
 router.get("/current", protect, getUser);
+router.patch("/update", protect, upload.single("avatar"), updateUser);
 
 export default router;
