24bc9fc34999bc9ecf34716b37e4efbf9ed744d7
MarekLempart <kama.zakupy@gmail.com>
2025-03-25 19:41:40 +0100

    fix

--- src/sockets/gameRoomSocket.ts
+++ src/sockets/gameRoomSocket.ts
@@ -31,8 +31,12 @@ interface ChatMessageData {
 export function initGameRoomSocket(server: any) {
     const io = new Server(server, {
         cors: {
-            origin: ["http://localhost:8081", "http://localhost:5173", "http://localhost:3000"],
-            methods: ["GET", "POST"],
+            origin: [
+                "http://localhost:5173",
+                "http://localhost:8081",
+                "http://localhost:3000",
+            ],
+            methods: ["GET", "POST", "PATCH", "DELETE"],
         },
     });
 
